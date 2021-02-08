ab95d6236f34dc31df49b1b222402c816fe99e5e
Carlos Morfin Diaz <carlosmorfindiaz@gmail.com>
2021-02-08 10:12:10 -0600

    Add strategie passwordless

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    root 'users/sessions#new'
+    get 'email_confirmation', to: 'users/sessions#redirect_from_magic_link'
+    post 'sign_in_with_token', to: 'users/sessions#sign_in_with_token'
+  end
 end
